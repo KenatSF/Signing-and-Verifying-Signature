66749b4e58f83a4c299d310551b393c068a35c69
KenatSF <banerabh@gmail.com>
2022-08-20 17:42:27 -0500

    Sign a message test update

--- test/VerifySignature.test.js
+++ test/VerifySignature.test.js
@@ -28,47 +28,91 @@ contract("Verify Signature", () => {
     //                Finally, recover the signer with ecrecover() inside the contract making use of Solidity.
 
     console.log('-------------------------------------------------------------------------------');
-    console.log("Getting message Hash.")
+    console.log("Getting message Hash from contract.")
     const hash = await verify.getMessageHash(_address, _amount, _message, _nonce, {from: accounts[0]});
     console.log(hash);
     console.log(`Type of: ${typeof(hash)}`);
     console.log(" ");
 
     console.log('-------------------------------------------------------------------------------');
-    console.log("Getting ANOTHER message Hash.")
+    console.log("Getting message Hash from web3.")
     const hash1 = await web3.utils.soliditySha3(_address, _amount, _message, _nonce);
     console.log(hash1);
     console.log(`Type of: ${typeof(hash1)}`);
     console.log(" ");
-
+    
     
 
-    console.log('-------------------------------------------------------------------------------');
+
+    console.log('-------------------------------------------------------------------------------');   // This method works inside de App but here, It does not
+    console.log("Signing message Hash with.")
+    const msgHash = web3.eth.accounts.hashMessage(hash);
+    const signed_hash0 = await web3.eth.sign(msgHash, accounts[0]);
+    console.log(msgHash);
+    console.log(signed_hash0);
+    console.log(" ");
+
+    console.log('-------------------------------------------------------------------------------');   // The last byte is different from below
+    console.log("Signing message Hash without.")
+    const signed_hash1 = await web3.eth.sign(hash, accounts[0]);
+    console.log(signed_hash1);
+    console.log(" ");
+
+
+    console.log('-------------------------------------------------------------------------------');   // This method requires the private_key
     console.log("Signing message Hash.")
-    const signed_hash = await web3.eth.sign(hash, accounts[0]);
-    console.log(signed_hash);
-    console.log(" ");
-
-
-    console.log('-------------------------------------------------------------------------------');
-    console.log("Signing ANOTHER message Hash.")
-    const signed_hash1 = await web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_ACCOUNT_0);
-    console.log(signed_hash1.signature);
+    const signed_hash2 = await web3.eth.accounts.sign(hash, process.env.PRIVATE_KEY_ACCOUNT_0);
+    console.log(signed_hash2);
+    console.log(signed_hash2.signature);
     console.log(" ");
     
 
 
-    console.log('-------------------------------------------------------------------------------');     // Note: The last byte is different.
+    console.log(" Verify within smart-contract");
+    console.log('-------------------------------------------------------------------------------');     
     console.log("Verifying signer!");
-    const signer = await verify.verifySigner(_address, _amount, _message, _nonce, signed_hash, {from: accounts[9]});
-    console.log(`Signer: ${signer}`);
+    const signer0 = await verify.verifySigner(_address, _amount, _message, _nonce, signed_hash0, {from: accounts[9]});
+    console.log(`Signer: ${signer0}`);
     console.log(" ");
 
-    console.log('-------------------------------------------------------------------------------');
+    console.log('-------------------------------------------------------------------------------');     
     console.log("Verifying signer!");
-    const signer1 = await verify.verifySigner(_address, _amount, _message, _nonce, signed_hash1.signature, {from: accounts[9]});
+    const signer1 = await verify.verifySigner(_address, _amount, _message, _nonce, signed_hash1, {from: accounts[9]});
     console.log(`Signer: ${signer1}`);
     console.log(" ");
+
+    console.log('-------------------------------------------------------------------------------');
+    console.log("Verifying signer!");
+    const signer2 = await verify.verifySigner(_address, _amount, _message, _nonce, signed_hash2.signature, {from: accounts[9]});
+    console.log(`Signer: ${signer2}`);
+    console.log(" ");
+
+
+    console.log(" Verify within javascript");
+    console.log('-------------------------------------------------------------------------------');   
+    console.log("Verifying signer!");
+    const a =  web3.eth.accounts.recover(hash, signed_hash0, {from: accounts[9]});
+    console.log(`Signer: ${a}`);
+    console.log(" ");
+
+    console.log('-------------------------------------------------------------------------------');   // Same answer that const d
+    console.log("Verifying signer!");
+    const b =  web3.eth.accounts.recover(hash, signed_hash1, {from: accounts[9]});
+    console.log(`Signer: ${b}`);
+    console.log(" ");
+
+    console.log('-------------------------------------------------------------------------------');   
+    console.log("Verifying signer!");
+    const c =  web3.eth.accounts.recover(signed_hash2, {from: accounts[9]});
+    console.log(`Signer: ${c}`);
+    console.log(" ");
+
+    console.log('-------------------------------------------------------------------------------'); //This method doesn't work eventhough hash and signature are correct
+    console.log("Verifying signer!");
+    const d =  web3.eth.accounts.recover(hash, signed_hash2.signature, {from: accounts[9]});
+    console.log(`Signer: ${d}`);
+    console.log(" ");
+
 
   
   });
